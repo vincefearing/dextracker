0195b4221d95824fcff050a8c311a4d7494a4e30
Vincent Fearing <vincefearing@gmail.com>
2025-07-22 03:33:20 +0000

    Add endpoint to list all Pokemon

--- api/main.go
+++ api/main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	http.HandleFunc("/pokemon/", rootHandler)
+	http.HandleFunc("/pokemon", getAllPokemonHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -38,4 +39,40 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(p)
+}
+
+func getAllPokemonHandler(w http.ResponseWriter, r *http.Request) {
+	files, err := os.ReadDir("../data")
+
+	if err != nil {
+		log.Printf("Failed to read directory: %v", err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+
+	type PokemonListItem struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+
+	pokemonList := []PokemonListItem{}
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			continue
+		}
+
+		file_name := file.Name()
+		name := strings.TrimSuffix(file_name, ".json")
+
+		item := PokemonListItem{
+			Name: name,
+			URL:  "/pokemon/" + name,
+		}
+
+		pokemonList = append(pokemonList, item)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pokemonList)
 }
